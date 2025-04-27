2c94487818d994f6b502b2edbace61f55ddd2eed
Yash Choudhary <mailtoyash16@gmail.com>
2025-04-27 18:07:07 +0530

    First commit

---
+++ Intro.jsx
@@ -0,0 +1,31 @@
+// Why learn React?
+// hype, job, trend, build UI
+// makes easy to manage and build complex front end
+
+// Why should I learn?
+// After mastering JS, most project don't need react in Initial phase
+
+// Why react was created
+// Phantom Message problem
+
+// State - JS , UI - DOM
+// There should be sync between both
+
+// React is a library, not framework (framework has strict rules like military and Library has more freedom more like a cool dude)
+// core of React (state or UI Manipulation, JSX -JS and HTML in one file)
+// How to propogate changes - Hooks
+// Reusing of component - Properties (key : value)
+
+// Additional add-on to React
+/*  - Router
+    - State Management
+        - Redux , Redux toolkit, zustand, context API
+    - Class based component
+        - Legacy code
+    - BAAS apps (Backend as a Service)
+
+   After React
+    - React is no complete solution, no SEO, browser render of JS problem, no routing
+    - Framework - NextJS, Gatsby , Remix
+
+*/
